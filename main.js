b3c0c8a47f8c5d9a4785351ec38e7d706044c4c5
psjh9376284 <psjh9376284@gmail.com>
2013-10-19 16:47:34 +0800

    fix #12

--- main.js
+++ main.js
@@ -2,30 +2,41 @@ var gameModule = (function (){
 	
 	var setTimeoutVar,
 		count = 0;
-	var color=["black","red","green","pink","orange","purple","yellow","blue","gray","brown"];
 	function start(){
 
 		var canvas = document.getElementById('game'),
 		 	ctx = canvas.getContext('2d'),
 		 	ballX = Math.floor(Math.random() * 300),
 		 	ballY = Math.floor(Math.random() * 500),
-		 	ballR = Math.floor(Math.random() * 100);
+		 	ballR = 10+Math.floor(Math.random() * 90),
+		 	r = Math.floor(Math.random() * 255),
+		 	g = Math.floor(Math.random() * 255),
+		 	b = Math.floor(Math.random() * 255),
+		 	second = 500+Math.floor(Math.random() * 1500);
 
 		canvas.width = 480;
-		canvas.height = 320;
+		canvas.height = 640;
 
-		ctx.fillStyle = color[Math.floor(Math.random()*9)];
+		ctx.fillStyle = "rgb("+r+","+g+","+b+")";
+
 		ctx.beginPath();
 		ctx.arc( ballX, ballY, ballR, 0, Math.PI * 2, true);
+
 		ctx.fill();
 
 		if(count >= 10){
 
 		}
 		else{
-			setTimeoutVar = setTimeout(start,1000);
+			if(ballR >= 50){
+				setTimeoutVar = setTimeout(start,second);
+			}
+			else{
+				setTimeoutVar = setTimeout(start,second);
+			}				
 			count++;
 			console.log("count ="+count);
+
 		}
 
 	}
