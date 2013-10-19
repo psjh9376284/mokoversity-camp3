05ba5a5721b8e118a3262748c6069f5ce9318b7c
psjh9376284 <psjh9376284@gmail.com>
2013-10-19 15:18:29 +0800

    fix #10

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 var gameModule = (function (){
-
+	
+	var setTimeoutVar,
+		count = 0;
 	function start(){
 
-	var canvas = document.getElementById('game'),
-		 ctx = canvas.getContext('2d'),
-		 ballX = Math.floor(Math.random() * 300),
-		 ballY = Math.floor(Math.random() * 500),
-		 ballR = Math.floor(Math.random() * 100);
+		var canvas = document.getElementById('game'),
+		 	ctx = canvas.getContext('2d'),
+		 	ballX = Math.floor(Math.random() * 300),
+		 	ballY = Math.floor(Math.random() * 500),
+		 	ballR = Math.floor(Math.random() * 100);
 
-	canvas.width = 480;
-	canvas.height = 320;
+		canvas.width = 480;
+		canvas.height = 320;
 
-	ctx.fillStyle = 'black';
-	ctx.beginPath();
-	ctx.arc( ballX, ballY, ballR, 0, Math.PI * 2, true);
-	ctx.fill();
+		ctx.fillStyle = 'black';
+		ctx.beginPath();
+		ctx.arc( ballX, ballY, ballR, 0, Math.PI * 2, true);
+		ctx.fill();
+
+		if(count >= 10){
+
+		}
+		else{
+			setTimeoutVar = setTimeout(start,1000);
+			count++;
+			console.log("count ="+count);
+		}
+
+
 	}
 
 	return {
