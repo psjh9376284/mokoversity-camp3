adc3c2a58960d4912b8499c5fdd252df307b9786
psjh9376284 <psjh9376284@gmail.com>
2013-10-20 14:22:02 +0800

    fix #19

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,13 @@
 /*
  * GET users listing.
  */
+var scores = [];
 
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
 
 exports.save = function(req, res){
-  console.log("Your scores:" + req.url);
+  scores.push(req.query.score);
+  console.log("Your scores:" + scores);
 };
